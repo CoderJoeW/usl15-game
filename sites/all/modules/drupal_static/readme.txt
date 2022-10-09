3adee041f975ba0366d858816cb07fffbfdbd81a
jwilliamson <j.williamson@gambler-labs.com>
2022-10-09 14:32:26 -0400

    Burn it all to the ground

--- sites/all/modules/drupal_static/readme.txt
+++
@@ -1,3 +0,0 @@
-Drupal Static backport.
-
-All code is in 6.x-1.x.
--- sites/all/modules/ziquid_game/js/zg_bounce.js
+++
@@ -1,7 +0,0 @@
-Drupal.behaviors.zg_bounce = function (context) {
-
-  function clickToContinue() {
-    window.location.href = $('.button-continue').attr('href');
-  }
-  setTimeout(clickToContinue, 3600);
-};
