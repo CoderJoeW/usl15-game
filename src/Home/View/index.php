ffc22fdbbad17796dafa16d739d128c9f6aa4480
jwilliamson <j.williamson@gambler-labs.com>
2022-10-10 21:11:45 -0400

    finish main nav

--- src/Home/View/index.php
+++ src/Home/View/index.php
@@ -1,7 +1,3 @@
-<?php
-use Usl\Hom\View;
-?>
-
 <style>
     li{
         /*background-color: var(--color-primary);*/
@@ -15,15 +11,51 @@ use Usl\Hom\View;
 </style>
 
 <div class="container">
-<ul class="grid gap-xs">
-    <li class="col-4">770 Strength</li>
-    <li class="col-5 text-center">3/3@</li>
-    <li class="col-3 text-right float-right">75/108XP</li> 
-</ul>
+    <ul class="grid gap-xs">
+        <li class="col-4"></li>
+        <li class="col-4"></li>
+    </ul>
 
-<ul class="grid gap-xs">
-    <li class="col-4">770 Strength</li>
-    <li class="col-5 text-center">3/3@</li>
-    <li class="col-3 text-right float-right">75/108XP</li> 
-</ul>
+    <ul class="grid gap-xs">
+        <li class="col-4"></li>
+        <li class="col-4 text-center">Missions</li>
+    </ul>
+
+    <br>
+
+    <ul class="grid gap-xs">
+        <li class="col-1"></li>
+        <li class="col-4 text-center">Debates</li>
+        <li class="col-2"></li>
+        <li class="col-4 text-center">Aides</li> 
+    </ul>
+
+    <ul class="grid gap-xs">
+        <li class="col-1"></li>
+        <li class="col-4 text-center">Elections</li>
+        <li class="col-2"></li>
+        <li class="col-4 text-center">Actions</li> 
+    </ul>
+
+    <br>
+
+    <ul class="grid gap-xs">
+        <li class="col-4"></li>
+        <li class="col-4 text-center">Move</li>
+    </ul>
+
+    <ul class="grid gap-xs">
+        <li class="col-4"></li>
+        <li class="col-4 text-center">Elders</li>
+    </ul>
+
+    <br>
+    <br>
+
+    <ul class="grid gap-xs">
+        <li class="col-3"style="margin-left: 25px">Profile</li>
+        <li class="col-3">Clan</li>
+        <li class="col-3">Help</li>
+        <li class="col-2">Forum</li>
+    </ul>
 </div>
